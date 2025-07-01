053de907e5f071291164d94a3ceb35f93251866c
ltizzi <terlizzileonardo@gmail.com>
2025-07-01 19:17:44 -0300

    updated user profile component

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -3,16 +3,16 @@ import BaseButton from "./common/BaseButton";
 import { AppContext } from "../context/AppContext";
 
 const UserProfile = () => {
-  const { isAdmin, setAdmin } = useContext(AppContext);
+  const { user } = useContext(AppContext);
   return (
     <div className="flex flex-col justify-center items-center align-middle h-screen">
-      <div className="flex flex-col justify-center text-center rounded-xl py-5 px-7 bg-base-100">
-        <h1 className="text-white text-4xl">This is the user profile</h1>
-        <BaseButton
-          btnLabel={isAdmin ? "Admin off" : "Admin on"}
-          btnAction={setAdmin}
-          btnType={isAdmin ? "error" : "success"}
-        />
+      <div className="flex flex-col justify-center text-center items-center gap-4 w-1/3 rounded-xl py-10 px-7 bg-base-100/80">
+        <h1 className="text-white text-5xl font-bold">
+          {user.nickname} profile
+        </h1>
+        <img src={user.avatar} className="rounded-full w-40" alt="" />
+        <p className="text-3xl font-light">{user.name + " " + user.lastname}</p>
+        <p className="text-xl font-extralight italic">{user.email}</p>
       </div>
     </div>
   );
